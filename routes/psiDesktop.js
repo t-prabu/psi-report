4e03b3f9567e8d18dd29da4efc6c97e22a14b469
Prabu <prabuisthebest@gmail.com>
2018-08-30 16:30:47 +0530

    init

---
+++ routes/psiDesktop.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+const psi = require('psi-v4');
+
+function psiDefault(url, req, res, next){
+     
+// Get the PageSpeed Insights report
+// psi(url).then(data => {
+//   console.log(data.ruleGroups.SPEED.score);
+//   console.log(data.pageStats);
+// });
+ 
+// Output a formatted report to the terminal
+// psi.output(url).then(() => {
+//   console.log('done');
+// });
+ 
+// Supply options to PSI and get back speed and usability scores
+psi(url, {nokey: 'true', strategy: 'desktop'}).then(data =>data.data).then(function(data){res.json(data)
+    }).catch(next)
+
+
+}
+
+/* GET users listing. */
+router.all('/', function(req, res, next) {
+    // console.log(req.body.url);
+    // console.log(psiDefault(req.body.url));
+  psiDefault(req.body.url, req, res, next);
+
+
+});
+
+module.exports = router;
